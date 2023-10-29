3369b143cb8d0a0e32e523a776e7addd04323309
Lwasinam <lwasinamdilli@gmail.com>
2023-10-29 19:51:15 +0100

    addition

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ def get_config():
         "batch_size":2,
         "num_epochs": 20,
         "lr": 10**-4,
-        "seq_len": 350,
+        "seq_len": 150,
         "d_model": 512,
         "lang_src": "0",
         "lang_tgt": "1",
--- model.py
+++ model.py
@@ -206,7 +206,7 @@ class Decoder(nn.Module):
 
 
 class Transformer(nn.Module):
-    def __init__(self, seq_len:int, batch:int, d_model:int,target_vocab_size:int, source_vocab_size:int, head: int = 8, d_ff: int =  2048, number_of_block: int = 4) -> None:
+    def __init__(self, seq_len:int, batch:int, d_model:int,target_vocab_size:int, source_vocab_size:int, head: int = 8, d_ff: int =  2048, number_of_block: int = 6) -> None:
         super(Transformer, self).__init__()
     
        
